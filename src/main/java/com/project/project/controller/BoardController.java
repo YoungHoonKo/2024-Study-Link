f7c8dbfbaca2b636618e4d1f43fe14b32c015b4b
parkddangkong <pkr0957@naver.com>
2024-08-05 22:29:20 +0900

    최종합본 2차, 8월 5일 - 경로 수정 및 패키징 변경

--- src/main/java/com/project/project/controller/BoardController.java
+++ src/main/java/com/project/project/controller/BoardController.java
@@ -20,14 +20,15 @@ public class BoardController {
 
     @GetMapping("/save")
     public String saveFrom(){
-        return "save";
+        System.out.println("들어옴");
+        return "Board/save"; // 경로 수정
     }
 
     @PostMapping("/save")
     public String save(@ModelAttribute BoardDTO boardDTO) {
         System.out.println("boardDTO = " + boardDTO);
         boardService.save(boardDTO);
-        return "index";
+        return "Board/index"; // 경로 수정
     }
 
     @GetMapping("/")
@@ -35,33 +36,33 @@ public class BoardController {
         // DB에서 전체 게시글 데이터를 가져와서 list.html에 보여준다.
         List<BoardDTO> boardDTOList = boardService.findAll();
         model.addAttribute("boardList", boardDTOList);
-        return "list";
+        return "Board/list"; // 경로 수정
     }
 
     @GetMapping("/{id}")
     public String findById(@PathVariable Long id, Model model){
         /*
-        * 해당 게시글의 조회수를 하나 올리고
-        * 게시글 데이터를 가져와서 detail.html에 출력
-        * */
+         * 해당 게시글의 조회수를 하나 올리고
+         * 게시글 데이터를 가져와서 detail.html에 출력
+         * */
         boardService.updateHits(id);
         BoardDTO boardDTO = boardService.findById(id);
         model.addAttribute("board", boardDTO);
-        return "detail";
+        return "Board/detail"; // 경로 수정
     }
 
     @GetMapping("/update/{id}")
     public String updateFrom(@PathVariable Long id, Model model){
         BoardDTO boardDTO = boardService.findById(id);
-        model.addAttribute("boardUpdate",boardDTO);
-        return "update";
+        model.addAttribute("boardUpdate", boardDTO);
+        return "Board/update"; // 경로 수정
     }
+
     @PostMapping("/update")
     public String update(@ModelAttribute BoardDTO boardDTO, Model model) {
         BoardDTO board = boardService.update(boardDTO);
         model.addAttribute("board", board);
-        return "detail";
+        return "Board/detail"; // 경로 수정
 //        return "redirect:/board/" + boardDTO.getId();
     }
-
 }
