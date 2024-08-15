4f9935e46eb11a23a16181f10f8d54aed6cd8738
bibisam06 <147711078+bibisam06@users.noreply.github.com>
2024-08-15 15:52:09 +0900

    dddd

--- src/main/resources/static/js/Admin/admin.js
+++ src/main/resources/static/js/Admin/admin.js
@@ -13,18 +13,21 @@ document.addEventListener('DOMContentLoaded', function() {
             });
 
             if (response.ok) {
-                //FIXME : response 값이 html 로 반환됨.
+
                 const contentType = response.headers.get('content-type');
-                if (contentType && contentType.includes('application/json')) {
-                    const data = await response.json();  // JSON으로 응답을 처리
-                    console.log('Role data:', data);
-                    return data.role;  // 서버 응답이 JSON 객체라면 data.role로 접근
-                } else {
-                    // 예상하지 못한 형식의 응답일 경우
-                    const errorText = await response.text();  // 응답을 텍스트로 읽기
-                    console.error('Unexpected response format:', errorText);
-                    return null;
-                }
+                const data = await response.json();  // JSON으로 응답을 처리
+                console.log('Role data:', data);
+                return data.role;  // 서버 응답이 JSON 객체라면 data.role로 접근
+                // if (contentType && contentType.includes('application/json')) {
+                //     const data = await response.json();  // JSON으로 응답을 처리
+                //     console.log('Role data:', data);
+                //     return data.role;  // 서버 응답이 JSON 객체라면 data.role로 접근
+                // } else {
+                //     // 예상하지 못한 형식의 응답일 경우
+                //     const errorText = await response.text();  // 응답을 텍스트로 읽기
+                //     console.error('Unexpected response format:', errorText);
+                //     return null;
+                // }
             } else {
                 console.error('Role check failed:', response.status);
                 return null;
@@ -48,6 +51,7 @@ document.addEventListener('DOMContentLoaded', function() {
         adminContent.style.display = 'block';
         memberList.style.display = 'block';
         // if (role && role.includes("ROLE_ADMIN")) {
+        //
         //     adminTitle.textContent = '관리자 페이지에 오신 것을 환영합니다';
         //     adminContent.style.display = 'block';
         //     memberList.style.display = 'block'; // 권한이 확인되면 버튼들이 보이도록 설정
