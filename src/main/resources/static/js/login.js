cc6a463230a56efe818a285a3185cf1655f9b6ae
bibisam06 <147711078+bibisam06@users.noreply.github.com>
2024-08-23 17:30:34 +0900

    수정

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -3,9 +3,9 @@ document.getElementById('submit').addEventListener('click', function(event) {
 
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
+
     const messageElement = document.getElementById('message');
 
-    // 사용자 이름과 비밀번호가 모두 입력되지 않았을 경우
     if (!username || !password) {
         messageElement.textContent = '아이디와 비밀번호를 입력하세요';
         messageElement.style.color = 'red';
@@ -17,44 +17,26 @@ document.getElementById('submit').addEventListener('click', function(event) {
         password: password
     };
 
-    fetch('/api/admin/login', {
+    fetch('/login', {
         method: 'POST',
         headers: {
-            Accept: 'application/json',
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(loginData)
     })
-        .then(response => {
-            if (!response.ok) {
-                return response.text().then(text => {
-                    throw new Error(`HTTP error! status: ${response.status}, response: ${text}`);
-                });
-            }
-            return response.json(); // 응답을 JSON으로 변환 (응답 본문을 JSON으로 파싱)
-        })
+        .then(response => response)
         .then(data => {
-            if (!data.access) {
-                alert('로그인 오류');
-                window.location.reload();
-                return;
+            if (data.headers.get("access") == null){
+                alert("로그인 오류")
+                window.location.reload()
+                return
             }
-
             if (data.ok) {
-                const accessToken = data.access;
+                const accessToken = data.headers.get("access");
                 messageElement.textContent = 'Login successful!';
                 messageElement.style.color = 'green';
-                console.log(data.role);
                 localStorage.setItem('access', accessToken);
-
-                // 역할에 따라 리다이렉트
-                if (data.role === 'ROLE_ADMIN') {
-                    window.location.href = '/admin';
-                } else if (data.role === 'ROLE_USER') {
-                    window.location.href = '/'; // 첫화면으로 리다이렉트
-                } else {
-                    window.location.href = '/404';
-                }
+                window.location.href = "/";
             } else {
                 messageElement.textContent = 'Login failed: 아이디와 비밀번호가 틀립니다.';
                 messageElement.style.color = 'red';
@@ -64,4 +46,42 @@ document.getElementById('submit').addEventListener('click', function(event) {
             messageElement.textContent = '오류가 발생했습니다: ' + error.message;
             messageElement.style.color = 'red';
         });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    // 로그인이 되어있는지 확인
+    const accessToken = localStorage.getItem('access');
+
+    if (accessToken) {
+        // 로그인 상태이면 /로 리다이렉트
+        fetch('/api/auth/validate-token', {
+            method: 'POST',
+            headers: {
+                'access': accessToken
+            }
+        })
+            .then(response => {
+                if (response.ok) {
+                    window.location.href = "/";
+                }
+            })
+            .catch(error => {
+                console.error('Error checking auth status:', error);
+                // 필요한 경우 오류 처리
+            });
+    }
+
+    var dropdowns = document.querySelectorAll('.dropdown');
+    dropdowns.forEach(function(dropdown) {
+        dropdown.addEventListener('mouseenter', function() {
+            this.querySelector('.dropdown-content').style.display = 'block';
+        });
+        dropdown.addEventListener('mouseleave', function() {
+            this.querySelector('.dropdown-content').style.display = 'none';
+        });
+    });
+
+    document.getElementById('logo').addEventListener('click', function() {
+        window.location.href = '/';
+    });
 });
